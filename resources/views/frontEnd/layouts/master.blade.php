80f6d59bde50a68fca98f8ee48ccc77e627494da
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-06-15 17:18:44 +0600

    pacage added

--- resources/views/frontEnd/layouts/master.blade.php
+++ resources/views/frontEnd/layouts/master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practice Project </title>
-
-    <link rel="stylesheet" href="{{ asset('fontEnd/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/frontEnd/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('public/frontEnd/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/frontEnd/css/all.min.css') }}">
 </head>
+
 <body>
     <nav>
         <ul>
@@ -18,18 +21,21 @@
     <div class="container">
         @yield('content')
     </div>
-<footer>
-    <p>&copy; {{ date('Y') }} Practice Project. All rights reserved.</p>
-    <p>Developed by <a href="" target="_blank">Your Name</a></p>
-    <p>Follow us on 
-        <a href="https://www.facebook.com" target="_blank">Facebook</a>, 
-        <a href="https://www.twitter.com" target="_blank">Twitter</a>, 
-        <a href="https://www.instagram.com" target="_blank">Instagram</a>
-</footer>
+    <footer>
+        <p>&copy; {{ date('Y') }} Practice Project. All rights reserved.</p>
+        <p>Developed by <a href="" target="_blank">Your Name</a></p>
+        <p>Follow us on
+            <a href="https://www.facebook.com" target="_blank">Facebook</a>,
+            <a href="https://www.twitter.com" target="_blank">Twitter</a>,
+            <a href="https://www.instagram.com" target="_blank">Instagram</a>
+    </footer>
 
-
-    <script src="{{ asset('frontEnd/js/scrpit.js') }}"></script>
+    <script src="{{ asset('public/frontEnd/js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('public/frontEnd/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('public/frontEnd/js/scrpit.js') }}"></script>
+    <script src="{{ asset('public/frontEnd/js/all.min.js') }}"></script>
     @yield('scripts')
 
 </body>
+
 </html>
